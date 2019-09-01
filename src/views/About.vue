e99bd72e7f7c92a7f602a670f17357ca8c78b624
Midias <midias.dev@gmail.com>
2019-09-01 10:52:39 +0200

    Apollo client + graphql uninstalled

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <ApolloExample></ApolloExample>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import Component  from 'vue-class-component';
 import ApolloExample from '@/components/ApolloExample.vue';
-@Component({
-  components: ApolloExample
-})
+@Component({})
 export default class About extends Vue {
 
 }
